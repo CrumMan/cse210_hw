9577ea7301ffcfeda7cfd88a4976fc5254c2a7fe
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-09-01 22:44:20 -0500

    Exercize 5 completed

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayResult();
+    }
+    static void DisplayWelcome()
+    {
+        System.Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        System.Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+    static int PromptUserNumber()
+    {
+        System.Console.Write("Please enter your faveroite number: ");
+        string numS = Console.ReadLine();
+        while (true)
+        {
+            if (int.TryParse(numS, out int num))
+            {
+                return num;
+            }
+            else
+            {
+                System.Console.Write("Please enter your faveroite number: ");
+                numS = Console.ReadLine();
+            }
+        }
+    }
+    static double SquareNumber(int num)
+    {
+        double numsqrt = num * num;
+        return numsqrt;
+    }
+    static void DisplayResult()
+    {
+        DisplayWelcome();
+        string name = PromptUserName();
+        int userNumber = PromptUserNumber();
+        double userNumberSQRT = SquareNumber(userNumber);
+        System.Console.WriteLine($"{name}, the square of your number is {userNumberSQRT}");
     }
 }
