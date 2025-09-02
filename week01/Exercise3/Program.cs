5976afd5213fce14329053964db2e33e070080a3
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-09-01 22:23:11 -0500

    3 and 4 of week 1 pushed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Runtime.CompilerServices;
+using Microsoft.Win32.SafeHandles;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random r = new Random();
+        int secretNumber = r.Next(1, 21);
+        int count = 1;
+        bool right = false;
+        System.Console.WriteLine("Guess the magic number!");
+        while (!right)
+        {
+            string stringGuess = Console.ReadLine();
+            if (int.TryParse(stringGuess, out int guess))
+            {
+                if (guess > secretNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < secretNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess == secretNumber)
+                {
+                    Console.WriteLine($"You guessed it! it took {count} guesses!");
+                    System.Console.WriteLine("Do you want to keep playing? yes / no");
+                    string play = Console.ReadLine();
+                    string lowerPlay = play.ToLower();
+                    if (lowerPlay == "y" || lowerPlay == "yes")
+                    {
+                        secretNumber = r.Next(1, 20);
+                    }
+                    else { right = true; }
+                }
+                count++;
+                if (!right)
+                {
+                    System.Console.WriteLine("What is your Guess? ");
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("NUMBERS ONLY.");
+            }
+        }
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,74 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        int number = 1;
+        List<int> numbersList = new List<int>();
+        System.Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        System.Console.Write("Enter number: ");
+        string numberString = Console.ReadLine();
+        if (numberString != "0")
+        {
+            while (number != 0)
+            {
+                if (int.TryParse(numberString, out number))
+                {
+                    if (number != 0)
+                        numbersList.Add(number);
+                    System.Console.Write("Enter number: ");
+                    numberString = Console.ReadLine();
+                    if (numberString == "0") { break; }
+                }
+                else
+                {
+                    System.Console.WriteLine("Enter a number");
+                    System.Console.Write("Enter number: ");
+                    numberString = Console.ReadLine();
+                }
+            }
+            int sum = 0;
+            int avg = 0;
+            double largestNumber = -999999999999999999;
+            double lpn = 999999999999999999;
+
+            foreach (int n in numbersList)
+            {
+                sum += n;
+                avg += n;
+                if (largestNumber < n)
+                {
+                    largestNumber = n;
+                }
+                if (lpn > n && n > 0)
+                {
+                    lpn = n;
+                }
+            }
+            string lpnstring = "";
+            if (lpn == 999999999999999999) { lpnstring = "None smaller than 0 "; }
+            else { lpnstring = $"{lpn}"; }
+
+            List<int> sortedNumbers = numbersList;
+            sortedNumbers.Sort();
+
+            avg /= numbersList.Count;
+            System.Console.WriteLine($"The sum is: {sum}");
+            System.Console.WriteLine($"The average is: {avg}");
+            System.Console.WriteLine($"The largest number is: {largestNumber}");
+            System.Console.WriteLine($"The smallest positive number is {lpnstring}");
+            System.Console.WriteLine("The Sorted list is: ");
+            foreach (int n in sortedNumbers)
+            {
+                System.Console.WriteLine(n);
+            }
+
+
+        }
     }
 }
